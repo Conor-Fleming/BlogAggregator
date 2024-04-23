54ef4a997e76eb2fa59ea05212eef87fa86529ca
Conor-Fleming <conorfleming03@gmail.com>
2024-04-22 18:48:41 -0700

    starting on some handler funcs and registering a couple endpoints

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 )
+
+type errorBody struct {
+	Error string `json:"error"`
+}
 
 func main() {
 	godotenv.Load(".env")
@@ -15,6 +20,10 @@ func main() {
 	mux := http.NewServeMux()
 
 	corsMux := corsMiddleware(mux)
+
+	mux.HandleFunc("/v1/readiness", readinessHandlerFunc)
+	mux.HandleFunc("/v1/err", errorHandlerFunc)
+
 	server := &http.Server{
 		Addr:    port,
 		Handler: corsMux,
@@ -33,5 +42,39 @@ func corsMiddleware(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+func readinessHandlerFunc(w http.ResponseWriter, r *http.Request) http.HandlerFunc{
+	return respondWithJSON(w, 200, payload{
+		"status": "ok"
+	})
+}
+
+func errorHandlerFunc(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	if payload != nil {
+		response, err := json.Marshal(payload)
+		if err != nil {
+			log.Println("error marshalling", err)
+			w.WriteHeader(500)
+			response, _ := json.Marshal(errorBody{
+				Error: "error marshalling",
+			})
+			w.Write(response)
+			return
+		}
+		w.WriteHeader(code)
+		w.Write(response)
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, errorBody{
+		Error: msg,
 	})
 }
