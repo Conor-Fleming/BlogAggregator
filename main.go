637930e6ca21bf1f9a259d50a3f5eb7238bca97c
Conor-Fleming <conorfleming03@gmail.com>
2024-04-22 18:27:35 -0700

    basic server listening on port 8080

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -10,4 +12,26 @@ func main() {
 	godotenv.Load(".env")
 	port := os.Getenv("PORT")
 
+	mux := http.NewServeMux()
+
+	corsMux := corsMiddleware(mux)
+	server := &http.Server{
+		Addr:    port,
+		Handler: corsMux,
+	}
+	log.Fatal(server.ListenAndServe())
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
